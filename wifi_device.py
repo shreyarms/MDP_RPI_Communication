616478f2cd460cf26eda994f0afc22bd2663cae5
Shannen <shannenlee21@yahoo.com>
2023-02-22 12:32:59 +0800

    latest changes

--- wifi_device.py
+++ wifi_device.py
@@ -1,3 +1,4 @@
+from image_tiling import imageTiling
 from wifi_communication import wifi_communication
 from model import model 
 import image_handling
@@ -19,29 +20,37 @@ w_recv.initiate_connection(client, sending_address) #8080
 w_send = wifi_communication(config.socket_buffer_size, config.terminating_str)
 w_send.initiate_connection(client, receiving_address) #8081
 
+num_of_pics_taken = 0
+num_of_boxes = 6
+counter = 1
+
 #2)algorithm 
 # algo_output = b"C:FL010000&5,3,NSEPC:FR010000&6,7,SSEPC:FS000010&8,9,WSEPC:END00000&8,9,W"
-if (w_recv.receive_message()=="BANANAS"):
-    algo_output = w_recv.receive_message()
-    w_send.send_message(algo_output)
 
-    byte_image = w_recv.receive_message()
-    image = byte_image.removeprefix(b"image:")
-    np_image = image_handling.bytes_to_np_array(image)
-    result_array = m.get_results(np_image)
-    print(result_array)
-    image = image_handling.draw_bbox(image_handling.np_array_to_image(np_image), result_array)
-    image.save("images/result_1.jpg")
-    im = Image.open("images/result_1.jpg")
-    im.show()
+# algo_output = b"C:FL010000&5,3,NSEPC:FR010000&6,7,SSEPC:FS000010&8,9,WSEPC:END00000&8,9,W"
+# w_send.send_message(algo_output)
 
-    classes = []
-    for result in result_array:
-        classes.append(result[1])
-    classes = "classes:"+ ','.join(classes)
+while(num_of_pics_taken != num_of_boxes):
+    if (w_recv.receive_message().startswith(b"image:")):
+        byte_image = w_recv.receive_message()
+        image = byte_image.removeprefix(b"image:")
+        np_image = image_handling.bytes_to_np_array(image)
+        result_array = m.get_results(np_image)
+        print(result_array)
+        image = image_handling.draw_bbox(image_handling.np_array_to_image(np_image), result_array)
+        image.save("images/result_" + str(counter) + ".jpg")
+        im = Image.open("images/result_" + str(counter) + ".jpg")
+        im.show()
 
-    classes = bytes(classes, 'utf-8')
-    w_send.send_message(classes)
+        classes = []
+        for result in result_array:
+            classes.append(result[1])
+        classes = "classes:"+ ','.join(classes)
 
-    w_send.disconnect()
-    w_recv.disconnect()
+        classes = bytes(classes, 'utf-8')
+        w_send.send_message(classes)
+        num_of_pics_taken += 1
+
+# imageTiling()
+w_send.disconnect()
+w_recv.disconnect()
